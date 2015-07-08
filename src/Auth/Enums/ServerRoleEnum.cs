f0d2cfbeffd87ceaf50bf920204b56ed36c1f40e
Skinz <oliviamarius35@gmail.com>
2015-07-08 18:06:15 +0200

    Poursuite du Auth

---
+++ src/Auth/Enums/ServerRoleEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Enums
+{
+    public enum ServerRoleEnum
+    {
+        Player = 1,
+        Animator = 2,
+        Moderator = 3,
+        Administrator = 4,
+        Fondator = 5
+    }
+}
