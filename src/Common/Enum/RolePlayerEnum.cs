c91b3200a71b12e626c7d660f3299275073faf58
Wax <waxmailer@gmail.com>
2015-07-09 18:44:59 +0200

    Ajout du RolePlayerEnum
    Enumeration désignant le role du compte.
    Joueur, Animateur, Modérateur, Administrateur ou Fondateur.

---
+++ src/Common/Enum/RolePlayerEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Enum
+{
+    public enum RolePlayerEnum
+    {
+        Player = 1,
+        Animator = 2,
+        Moderator = 3,
+        Administrator = 4,
+        Fondator = 5
+    }
+}
