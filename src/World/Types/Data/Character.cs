ab683b779f4edc22e62b141c83f4d72385f613f5
Skinz <oliviamarius35@gmail.com>
2015-07-12 21:55:03 +0200

    //

---
+++ src/World/Types/Data/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace World.Types.Data
+{
+    /// <summary>
+    /// Classe représentant le personnage connecté en jeu
+    /// </summary>
+    public class Character // hérité de player ou un équivalent pour des debugs ultérieur
+    {
+        #region Propreties
+        public int Id { get; private set; }
+        #endregion
+    }
+}
